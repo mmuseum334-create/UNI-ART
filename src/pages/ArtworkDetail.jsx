774ae984afc07b84ea53c2afdc3ba51ed997eee9
Nyck1975 <zambranoznick@gmail.com>
2026-04-06 17:30:15 -0500

    correción de color

--- src/pages/ArtworkDetail.jsx
+++ src/pages/ArtworkDetail.jsx
@@ -4,6 +4,9 @@
  */
 'use client'
 
+
+export const dynamic = 'force-dynamic';
+
 import { useState, useEffect } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import Link from 'next/link';
