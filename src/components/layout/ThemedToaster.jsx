2557db67442a030606e6e330cbcd35c902f7c41c
Nyck1975 <zambranoznick@gmail.com>
2026-04-13 02:09:05 -0500

    fix: descartar toasts al cambiar tema para evitar bg desactualizado

--- src/components/layout/ThemedToaster.jsx
+++ src/components/layout/ThemedToaster.jsx
@@ -1,10 +1,17 @@
 'use client';
 
-import { Toaster } from 'sileo';
+import { useEffect } from 'react';
+import { Toaster, sileo } from 'sileo';
 import { useTheme } from '@/contexts/ThemeContext';
 
 export default function ThemedToaster() {
   const { isDark } = useTheme();
+
+  // Al cambiar el tema, descarta toasts activos para evitar bg desactualizado
+  useEffect(() => {
+    sileo.dismiss();
+  }, [isDark]);
+
   return (
     <Toaster
       position="top-center"
